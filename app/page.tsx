9a48318e082fed91becb51944a7ebd7820ee8629
Soufian Ben Chakroune <soufianbenchakroune7@gmail.com>
2025-01-03 20:47:10 +0100

    add packages and finishin component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import QrCodeGenerator from "./QrCodeGenerator";
 
 export default function Home() {
   return (
     <div className="relative min-h-[100vh] h-full flex justify-center items-center">
+      <QrCodeGenerator />
       <Image
         src="/lightgraybackground.png"
         width={1600}
