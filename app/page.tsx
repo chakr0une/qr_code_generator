7b68f74f68337ba18d9262e020027a09a4c87f99
Soufian Ben Chakroune <soufianbenchakroune7@gmail.com>
2025-01-03 19:41:02 +0100

    add packgound

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="relative min-h-[100vh] h-full flex justify-center items-center">
+      <Image
+        src="/lightgraybackground.png"
+        width={1600}
+        height={1200}
+        alt="glass image"
+        className="fixed top-0 left-0 w-full h-full object-cover z-0 pointer-events-none"
+      />
+    </div>
+  );
+}
